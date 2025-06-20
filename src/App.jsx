331d2f1398b0b9c5826802d916d6eff924bbd6bf
ps3869253@gmail.com <ps3869253@gmail.com>
2025-06-20 19:21:01 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { BrowserRouter , Route,Routes} from "react-router-dom"
+import { NotFound } from "./pages/NotFound"
+import { Home } from "./pages/Home"
 function App() { 
   return (
     <div>
-      cheery ls strawbeery and vice-versa.
+      <BrowserRouter>
+      <Routes>
+        <Route path ="/" element={<Home/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
+
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+export function Home(){
+    return <div className="min-h-screen bg-gray-200">
+        hello
+    </div>
+}
